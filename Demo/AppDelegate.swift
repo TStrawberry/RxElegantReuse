b84088d9dc45669552d67a06f644d1550fb861e9
Todd <toddtang@dauron.com>
2018-09-29 17:12:51 +0800

    Add tvOS demo Demo switch to pure code

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let rootViewController = RootViewController(style: .plain)
+        let navigationViewController = UINavigationController(rootViewController: rootViewController)
+        
+        window?.rootViewController = navigationViewController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
